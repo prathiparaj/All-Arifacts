a5f5a8ff4e9c54b6f8fcd0ef5c9713227620093b
prathiparaj <prathiparaj1994@gmail.com>
2017-12-16 21:29:39 +0530

    klhxv

---
+++ Artifacts/mysql/mysql.ps1
@@ -0,0 +1,63 @@
+function Create-Folder {
+    Param ([string]$path)
+    if ((Test-Path $path) -eq $false) 
+    {
+        Write-Host "$path doesn't exist. Creating now.."
+        New-Item -ItemType "directory" -Path $path
+    }
+}
+
+function Download-File{
+    Param ([string]$src, [string] $dst)
+
+    (New-Object System.Net.WebClient).DownloadFile($src,$dst)
+    #Invoke-WebRequest $src -OutFile $dst
+}
+
+function WaitForFile($File) {
+  while(!(Test-Path $File)) {    
+    Start-Sleep -s 10;   
+  }  
+} 
+
+
+#Setup Folders
+
+$setupFolder = "c:\Software-Modules"
+Create-Folder "$setupFolder"
+
+#Create-Folder "c:\Spring-Framework"
+
+
+
+
+if((Test-Path "$setupFolder\mysql-5.7.20-winx64.zip") -eq $false)
+{
+  
+        Download-File "https://mylibrary123.blob.core.windows.net/reposit/mysql-5.7.20-winx64.zip" "$setupFolder\mysql-5.7.20-winx64.zip"  
+}
+
+
+
+
+
+
+Add-Type -assembly "system.io.compression.filesystem"
+
+$BackUpPath = "$setupFolder\mysql-5.7.20-winx64.zip"
+
+$destination = "C:\"
+
+
+
+Add-Type -assembly "system.io.compression.filesystem"
+
+[io.compression.zipfile]::ExtractToDirectory($BackUpPath, $destination)
+
+
+$Computer = $env:computername
+
+Invoke-command -Computername "$Computer" -scriptblock {
+cmd /c cd "C:\mysql-5.7.20-winx64\bin"
+cmd /c "mysqld --initialize"
+}
