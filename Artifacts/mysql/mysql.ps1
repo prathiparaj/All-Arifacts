19fea47b7f1e357cd7fdc1a6f6748b83f8084d7c
prathiparaj <33244470+prathiparaj@users.noreply.github.com>
2017-12-18 17:57:09 +0530

    Update mysql.ps1

--- Artifacts/mysql/mysql.ps1
+++ Artifacts/mysql/mysql.ps1
@@ -43,10 +43,13 @@ if((Test-Path "$setupFolder\sql.bat") -eq $false)
         Download-File "https://mylibrary123.blob.core.windows.net/reposit/sql.bat" "$setupFolder\sql.bat"  
 }
 
- $uri = "https://g7crtipl-my.sharepoint.com/personal/garima_anand_g7cr_in/_layouts/15/guestaccess.aspx?docid=1e9a817909d19495c815b07c5b6ce3af5&authkey=AUgASeIPUdmE44ngD_9vQ4M&e=fe8c9fb10ca24eca8814a323a3b3ba8d"
-$out = "c:\ToadEdge.msi"
-Invoke-WebRequest -uri $uri -OutFile $out
-Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $out /quiet /norestart /l c:\installlog.txt"  
+if((Test-Path "$setupFolder\vcredist_x64.exe") -eq $false)
+{
+  
+        Download-File "https://mylibrary123.blob.core.windows.net/reposit/sql.bat" "$setupFolder\sql.bat"  
+}
+
+   
 
 
 
