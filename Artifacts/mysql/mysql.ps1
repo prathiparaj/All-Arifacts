061993b24286c2859841d60817b894724db193d7
prathiparaj <33244470+prathiparaj@users.noreply.github.com>
2017-12-18 16:07:25 +0530

    Update mysql.ps1

--- Artifacts/mysql/mysql.ps1
+++ Artifacts/mysql/mysql.ps1
@@ -37,8 +37,16 @@ if((Test-Path "$setupFolder\mysql-5.7.20-winx64.zip") -eq $false)
         Download-File "https://mylibrary123.blob.core.windows.net/reposit/mysql-5.7.20-winx64.zip" "$setupFolder\mysql-5.7.20-winx64.zip"  
 }
 
+if((Test-Path "$setupFolder\sql.bat") -eq $false)
+{
+  
+        Download-File "https://mylibrary123.blob.core.windows.net/reposit/sql.bat" "$setupFolder\sql.bat"  
+}
 
-
+ $uri = "https://g7crtipl-my.sharepoint.com/personal/garima_anand_g7cr_in/_layouts/15/guestaccess.aspx?docid=1e9a817909d19495c815b07c5b6ce3af5&authkey=AUgASeIPUdmE44ngD_9vQ4M&e=fe8c9fb10ca24eca8814a323a3b3ba8d"
+$out = "c:\ToadEdge.msi"
+Invoke-WebRequest -uri $uri -OutFile $out
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $out /quiet /norestart /l c:\installlog.txt"  
 
 
 
@@ -56,11 +64,8 @@ Add-Type -assembly "system.io.compression.filesystem"
 
 
 
-if((Test-Path "$setupFolder\sql.bat") -eq $false)
-{
-  
-        Download-File "https://mylibrary123.blob.core.windows.net/reposit/sql.bat" "$setupFolder\sql.bat"  
-}
+
+
 
 
 Start-Process -FilePath $setupFolder\sql.bat
